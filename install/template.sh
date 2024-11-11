88c8b47a5b56b8fc09e79d4996572c1adf9cd24d
UK-Jimbo <126205015+UK-Jimbo@users.noreply.github.com>
2024-11-11 17:51:56 +0000

    Add a development template file

---
+++ install/template.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+# source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+# color
+# verb_ip6
+# catch_errors
+# setting_up_container
+# network_check
+# update_os
+
+source "../misc/install.func"
+color
+verb_ip6
+catch_errors
+
