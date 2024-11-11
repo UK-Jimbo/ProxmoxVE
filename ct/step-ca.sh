7003b9849aff39684cb895f23c994dffc14d65e0
UK-Jimbo <126205015+UK-Jimbo@users.noreply.github.com>
2024-11-11 20:49:27 +0000

    Update step-ca.sh

--- ct/step-ca.sh
+++ ct/step-ca.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/UK-Jimbo/ProxmoxVE/refs/heads/step-ca/misc/build.func)
-#source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# bash -c "$(wget -qLO - https://raw.githubusercontent.com/UK-Jimbo/ProxmoxVE/refs/heads/step-ca/ct/step-ca.sh)"
+
+# source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
