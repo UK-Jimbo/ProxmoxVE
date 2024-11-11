2acbc6950766a17c6fb1afe91e0a352493463694
UK-Jimbo <126205015+UK-Jimbo@users.noreply.github.com>
2024-11-11 18:15:45 +0000

    Updated Template

--- install/template.sh
+++ install/template.sh
@@ -5,13 +5,7 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-# This is my template for scripts and is intended to be used as follows
-# Create an LXC in proxmox and download this repo into the LXC
-# Write your script from 'Starting Script' to 'Script Finished'
-# When script is completed remove lines from source "../misc/install.func" to catch_errors
-# uncomment the lines from source to update_os
-# Remove these comments ^^^
-
+# Uncomment when script is complete
 # source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 # color
 # verb_ip6
@@ -20,8 +14,9 @@
 # network_check
 # update_os
 
+# Remove when script is complete
 source "../misc/install.func"
-VERBOSE="no"
+VERBOSE="yes"
 color
 verb_ip6
 catch_errors
@@ -30,11 +25,12 @@ msg_info "Starting Script..."
 Sleep 2
 msg_ok "Script Finished."
 
-motd_ssh
-customize
+# Uncomment when script is complete
+# motd_ssh
+# customize
 
-msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
-msg_ok "Cleaned"
+# msg_info "Cleaning up"
+# $STD apt-get -y autoremove
+# $STD apt-get -y autoclean
+# msg_ok "Cleaned"
 
