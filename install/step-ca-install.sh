147892c5b3c8bf594ba35151260c2a85cd493cec
UK-Jimbo <126205015+UK-Jimbo@users.noreply.github.com>
2024-11-16 06:31:01 +0000

    Added 10 Year Certificate lifetime

--- install/step-ca-install.sh
+++ install/step-ca-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2024 tteck
 # Author: tteck
-# Co-Author: MickLesk (Canbiz)
+# Co-Author: Jimbo
 # License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/Donkie/Spoolman
@@ -11,9 +11,9 @@ source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
-#setting_up_container
-#network_check
-#update_os
+# setting_up_container
+# network_check
+# update_os
 
 function generate_password () {
     set +o pipefail
@@ -23,6 +23,7 @@ function generate_password () {
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y sudo
+$STD apt-get install -y jq
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Step CA"
@@ -73,7 +74,8 @@ if [ "${STEPCA_INIT_REMOTE_MANAGEMENT}" == "true" ]; then
 fi
 
 $STD step ca init "${setup_args[@]}"
-echo ""
+
+# TODO:  Do we need this ?
 if [ "${STEPCA_INIT_REMOTE_MANAGEMENT}" == "true" ]; then
     echo "👉 Your CA administrative username is: ${STEPCA_INIT_ADMIN_SUBJECT}"
 fi
@@ -86,9 +88,12 @@ shred -u provisioner_password
 cp password /etc/step-ca/password.txt
 mv password $PWDPATH
 sudo chown -R step:step /etc/step-ca
+#cat <<< $(jq '.db.dataSource = "/etc/step-ca/db"' /etc/step-ca/config/ca.json) > /etc/step-ca/config/ca.json
+
 msg_ok "Installed Step-CA"
 
-msg_ok "Creating Service"
+msg_info "Creating Service"
+
 # Temporarily disable 'nounset' as throws error for $MAINPID
 set_state=$(set +o)
 set +u
@@ -152,6 +157,16 @@ EOF
 
 eval "$set_state"
 
+# Default to 10 Year Certificate
+jq '.authority += {
+        "claims": {
+            "minTLSCertDuration": "5m",
+            "maxTLSCertDuration": "87600h",
+            "defaultTLSCertDuration": "87600h"
+        }
+    }' "$CONFIGPATH" > tmp.json && mv tmp.json "$CONFIGPATH"
+
+
 $STD systemctl daemon-reload
 $STD systemctl enable --now step-ca
 msg_ok "Created Service"
@@ -159,13 +174,15 @@ msg_ok "Created Service"
 sleep 1
 export STEPPATH=/root/.step
 $STD step ca bootstrap --ca-url https://localhost:9000 --fingerprint $STEP_CA_FINGERPRINT --install
+
+# TODO: Check result? If not 'ok' throw error ?
 $STD step ca health
 
 motd_ssh
 customize
 
 
-# TODO: Remove files ?
+# TODO: Remove .deb files ?
 msg_info "Cleaning up"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
@@ -174,3 +191,22 @@ msg_ok "Cleaned"
 echo "Your CA administrative password is: ${STEP_CA_PROVISIONER_PASSWORD}"
 echo "Your password is: ${STEP_CA_PASSWORD}"
 echo "Your CA fingerprint is : ${STEP_CA_FINGERPRINT}"
+# echo ""
+# echo "You now need to trust this Root Certificate on your devices."
+# echo ""
+# echo "On macOS run the following commands in the terminal"
+# echo "/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""
+# echo "brew install step"
+# echo "step ca bootstrap --ca-url https://ca.lan:9000 --fingerprint ${STEP_CA_FINGERPRINT} --install"
+
+# TODO: Updates to build.func should not be in this branch
+
+
+# Your CA administrative password is: REp7BVKT8AAruvI2lhL3WHVvQkhQ87KsN2BJWecg
+# Your password is: nC1Ve2unABXbXQYgbXFIFLJfbGYm725I2t8E8cDy
+# Your CA fingerprint is : 3905bb72e10773391fbab35c9efdda7fcbc408e269d9c038370a857d13bc0bdd
+
+# On macOS run the following commands in the terminal
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install step
+step ca bootstrap --ca-url https://ca.lan:9000 --fingerprint 3905bb72e10773391fbab35c9efdda7fcbc408e269d9c038370a857d13bc0bdd --install
